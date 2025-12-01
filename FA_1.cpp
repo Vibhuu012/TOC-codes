40e3db2be6368bcf7dca083cfa20623eb2b869fa
Vibhuu012 <23_49051vibhu@dsc.du.ac.in>
2025-12-02 01:04:01 +0530

    Update FA_1.cpp

--- FA_1.cpp
+++ FA_1.cpp
@@ -1,3 +1,6 @@
+Design a Finite Automata (FA) that accepts all strings over S={0, 1} having three consecutive 1's as 
+a substring. Write a program to simulate this FA.
+
 #include <iostream>
 using namespace std;
 
