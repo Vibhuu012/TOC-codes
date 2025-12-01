c5d856930cb80c7fc0ee94dadc46e046e1b01f7d
Vibhuu012 <23_49051vibhu@dsc.du.ac.in>
2025-12-02 01:08:10 +0530

    Update q8.cpp

--- q8.cpp
+++ q8.cpp
@@ -1,5 +1,4 @@
-//Design a PDA and write a program for simulating the machine which accepts the language {wXwr| w 
-is any string over S={a, b} and wr is reverse of that string and X is a special symbol }.//
+//Design a PDA and write a program for simulating the machine which accepts the language {wXwr| w is any string over S={a, b} and wr is reverse of that string and X is a special symbol }.//
 
 #include <iostream>
 #include <stack>
